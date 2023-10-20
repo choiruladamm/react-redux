c03b4ad9c708beda4c1d39ae7e793e2382ebaaf3
choiruladamm <choiruladamm@gmail.com>
2023-10-20 23:21:41 +0700

    feat: add slice to the store

--- src/redux/store.tsx
+++ src/redux/store.tsx
@@ -1,5 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
+import counterReducer from "./countSlice";
 
 export const store = configureStore({
-  reducer: {},
+  reducer: counterReducer,
 });
