fd8c1e84b68cb3d40486f10ee245f758cd65da64
choiruladamm <choiruladamm@gmail.com>
2023-10-23 17:06:17 +0700

    feat: create store & provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,20 @@
-import React from "react";
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+
+import { createStore, applyMiddleware, Store } from "redux";
 import { Provider } from "react-redux";
-import { store } from "./redux/store.tsx";
+import thunk from "redux-thunk";
+import reducer from "./store/reducer.ts";
+
+const store: Store<ArticleState, ArticleAction> & {
+  dispatch: DispatchType;
+} = createStore(reducer, applyMiddleware(thunk));
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>
+  <Provider store={store}>
+    <App />
+  </Provider>
 );
